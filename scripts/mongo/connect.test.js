318f4bad1066a21f0fdb6823f0d9feff491c2a24
ruanjiayou <ruanjiayou123@gmail.com>
2019-08-11 18:18:52 +0700

    update

--- scripts/mongo/connect.test.js
+++ scripts/mongo/connect.test.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test?readPreference=primaryPreferred');
+mongoose.connect('mongodb://180.76.183.201:27017/test?readPreference=primaryPreferred');
 const model = mongoose.model('test', new mongoose.Schema({}, { strict: false }));
 (async()=>{
     let n = await model.find().count();
