e10a34a5683ac2196365439652f607e5cd8724c5
ruanjiayou <ruanjiayou123@gmail.com>
2020-01-10 22:50:46 +0700

    update

--- scripts/mongo/connect.test.js
+++ scripts/mongo/connect.test.js
@@ -1,7 +1,88 @@
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://root:123456@localhost:27017/novel?authSource=admin&readPreference=primaryPreferred');
+// const model = mongoose.model('resources', new mongoose.Schema({}, { strict: false }));
+// (async () => {
+// 	let len = 0, limit = 100, offset = 0;
+// 	do {
+// 	  console.log(len, limit);
+// 	  const result = await model.find({source_type:'article'}).select({'-content': 0}).limit(limit).skip(offset).lean(true);
+// 	  len += result.length;
+//       offset += limit;
+// 	  const buff = [];
+// 	  for(let i=0;i<result.length;i++){
+// 		const data = result[i];
+// 	    buff.push({
+// 		  updateOne: {
+// 		    filter: {source_type: data.source_type, source_id: data.source_id },
+// 			update: { $set: {id: data.source_id} },
+// 	        upsert: false,
+// 		  },
+// 		});
+// 	  };
+// 	  if(buff.length) {
+// 	    await model.bulkWrite(buff);
+// 	  }
+// 	} while(len === limit);
+// 	console.log('end')
+//     // let n = await model.find().count();
+//     // console.log(n);
+// })
+
+// const mongoose = require('mongoose');
+// const db1 = mongoose.createConnection('mongodb://root:123456@localhost:27017/novel?authSource=admin=readPreference=primaryPreferred');
+// console.log('start');
+// const sourceTable = db1.model('resources', new mongoose.Schema({}, { strict: false }));
+// (async () => {
+//   let len = 0,
+//     limit = 100,
+//     offset = 0;
+//   do {
+//     const results = await sourceTable.find({source_type:'article'}).select({'-content':1, 'source_id': 1}).limit(limit).skip(offset).lean(true);
+//     len = results.length;
+//     offset += len;
+//     const buff = [];
+//     results.forEach(data => {
+// 		buff.push({
+// 		  updateOne: {
+// 			filter: {source_type: 'article', source_id: data.source_id },
+// 			update: { $set: {id: data.source_id} },
+// 			upsert: false,
+// 		  },
+// 		});
+//     })
+//     if (buff.length) {
+//       await sourceTable.bulkWrite(buff)
+//     }
+//   } while (len === limit);
+//   console.log('同步完成')
+// })()
+
+// 修改group_id为id
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.99.100:27017/test?readPreference=primaryPreferred');
-const model = mongoose.model('test', new mongoose.Schema({}, { strict: false }));
+const db1 = mongoose.createConnection('mongodb://root:123456@localhost:27017/novel?authSource=admin=readPreference=primaryPreferred');
+console.log('start');
+const sourceTable = db1.model('group', new mongoose.Schema({}, { strict: false }));
 (async () => {
-    let n = await model.find().count();
-    console.log(n);
-})
+  let len = 0,
+    limit = 100,
+    offset = 0;
+  do {
+    const results = await sourceTable.find({}).limit(limit).skip(offset).lean(true);
+    len = results.length;
+    offset += len;
+    const buff = [];
+    results.forEach(data => {
+		buff.push({
+		  updateOne: {
+			filter: {group_id: data.group_id },
+			update: { $set: {id: data.group_id}, $unset: {group_id: 1} },
+			upsert: false,
+		  },
+		});
+    })
+    if (buff.length) {
+      await sourceTable.bulkWrite(buff)
+    }
+  } while (len === limit);
+  console.log('同步完成')
+})()
---
+++ ueditor/ueditor.md
@@ -0,0 +1,12 @@
+# ueditor
+- https://blog.csdn.net/qq_43258252/article/details/87859547
+- https://github.com/fex-team/ueditor
+- https://github.com/netpi/ueditor
+```js
+new CopyWebpackPlugin([
+  { from: 'src/asset/ueditor', to: 'ueditor' }
+])
+```
+- serverUrl
+- 后端 ueditor.config.json
+- use /ueditor/ue
