85dd626188d0136a2de408c7f9c4bd48b6addf7b
max <ruanjiayou@phoenixtv.com>
2021-10-19 18:20:01 +0800

    update

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,2 +1,28 @@
-console.log(process.env.STSTOKEN, 'token');
-console.log(JSON.parse(process.env.STSTOKEN), 'json');
+const express = require('express')
+const PNGlib = require('node-pnglib');
+const ngrok = require('ngrok');
+const app = express()
+const fs = require('fs');
+const path = require('path');
+const shelljs = require('shelljs');
+const bodyParser = require('body-parser');
+
+const filepath = path.normalize(__dirname + '/../../subscription-api/');
+app.use(bodyParser.json({ limit: '3mb' }));
+
+app.get('/test/png', async (req, res, next) => {
+  let png = new PNGlib(150, 150);
+  for (let i = 20; i < 100; i++) {
+    for (let j = 20; j < 100; j++) {
+      png.setPixel(i + 10, j + 25, '#cc0044');
+      png.setPixel(i + 20, j + 10, '#0044cc');
+      png.setPixel(i + 30, j, '#00cc44');
+    }
+  }
+  res.setHeader('Content-Type', 'image/png');
+  res.end(png.getBuffer());
+})
+
+app.listen(7003, function () {
+  console.log('express started at: 7003')
+})
