dd148a5536ff9ee0267a03656096f40cabe0a418
max <1439120442@qq.com>
2026-07-08 15:57:19 +0800

    etcd3

--- docker/etcd/notes.md
+++ docker/etcd/notes.md
@@ -4,3 +4,9 @@
 - docker compose up
 - 访问 `http://localhost:8002/index.html`
 - 测试连接: Host etcd-server, port 2379
+
+## 开启鉴权
+- 执行命令(distroless无法进入容器)
+  - 创建用户: `docker exec -it etcd-server etcdctl user add root --new-user-password=你的密码`
+  - 绑定用户角色: `docker exec -it etcd-server etcdctl user grant-role root root`
+  - 激活鉴权: `docker exec -it etcd-server etcdctl auth enable`
--- docker/etcd/test.js
+++ docker/etcd/test.js
@@ -9,11 +9,11 @@ const client = new Etcd3({
   // 如果在宿主机本地直接运行测试，填 'http://127.0.0.1:2379'
   hosts: 'http://127.0.0.1:2379',
   // 核心：配置账号密码认证
-  // auth: {
-  //   username: 'root',
-  //   password: '' // 替换为你设置的 root 密码
-  // }
-})
+  auth: {
+    username: 'root',
+    password: '123456' // 替换为你设置的 root 密码
+  }
+}).namespace('development/')
 async function getV(prefix) {
   const o = await client.getAll().prefix(prefix).strings();
   Object.keys(o).forEach(key => {
@@ -26,8 +26,8 @@ async function getV(prefix) {
         case 'number': v = info.value; break;
         case 'json': v = info.value; break;
       }
-      const k = key.replace(prefix + '/', '').trim().replace(/\//g, '.')
-      _.set(config, k, v);
+      const [, ...arr] = key.split('/');
+      _.set(config, arr, v);
     } catch (e) {
 
     }
@@ -41,18 +41,16 @@ async function main() {
     // 2. 首次获取配置 (Get)
     await getV('global');
     await getV('downloader-server');
-    console.log(`[初始配置] 当前的值为:`, config || '空(未设置)');
+    console.log(config, '[初始配置] 当前的值');
 
     // 3. 开启实时监听 (Watch)
-    console.log(`[监听启动] 开始实时监听 的变更...`);
+    console.log(`[监听启动] 开始实时监听变更...`);
 
-    const watcher = await client.watch().prefix('downloader-server').create();
+    const watcher = await client.watch().prefix('global').create();
 
     // 监听数据改变或新增 (Put 事件)
     watcher.on('put', (res) => {
-      console.log(`\n🚀 [配置变更通知] 检测到键值被修改或创建！`);
-      console.log(`键: ${res.key.toString()}`);
-      console.log(`新值: `, res.value.toJSON());
+      console.log(`🚀 [配置变更通知] 检测到键值被修改或创建！`, res.key.toString(), res.value.toString());
 
       // 在这里触发你业务系统的热更新逻辑，比如：
       // myDatabase.reconnect(res.value.toString());
@@ -60,8 +58,7 @@ async function main() {
 
     // 监听数据被删除 (Delete 事件)
     watcher.on('delete', (res) => {
-      console.log(`\n⚠️ [配置删除通知] 检测到键值被删除了！`);
-      console.log(`键: ${res.key.toString()}`);
+      console.log(`⚠️ [配置删除通知] 检测到键值被删除了！`, res.key.toString());
     });
 
     // 监听连接错误（防掉线重连）
@@ -79,6 +76,6 @@ main();
 // 保持进程不退出
 process.on('SIGINT', async () => {
   console.log('正在关闭 etcd 客户端...');
-  client.close();
+  // client.close();
   process.exit(0);
 });
